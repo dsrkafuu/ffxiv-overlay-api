44703752b3f098071edd09a94fb0614259cbc447
amzrk2 <amzrk2@outlook.com>
2020-08-12 22:11:03 +0800

    update: update declaration file

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,21 +3,16 @@ export as namespace OverlayAPI;
 export = OverlayAPI;
 
 declare class OverlayAPI {
-  constructor(events?: OverlayAPI.Events);
-  add(event: string, cbs: ((data: object) => any) | ((data: object) => any)[]): void;
-  remove(event: string, cb: ((data: object) => any) | number): void;
-  removeAll(event: string): void;
-  list(event: string): ((data: object) => any)[];
+  constructor(options?: OverlayAPI.Options);
+  addListener(event: string, cb: (data: object) => any): void;
+  removeListener(event: string, cb: (data: object) => any): void;
+  removeAllListener(event: string): void;
+  listListener(event: string): ((data: object) => any)[];
   call(msg: object): Promise<object>;
 }
 
 declare namespace OverlayAPI {
-  export interface Events {
-    CombatData?: ((data: object) => any) | ((data: object) => any)[];
-    LogLine?: ((data: object) => any) | ((data: object) => any)[];
-    ImportedLogLines?: ((data: object) => any) | ((data: object) => any)[];
-    ChangeZone?: ((data: object) => any) | ((data: object) => any)[];
-    ChangePrimaryPlayer?: ((data: object) => any) | ((data: object) => any)[];
-    OnlineStatusChanged?: ((data: object) => any) | ((data: object) => any)[];
+  export interface Options {
+    liteMode?: boolean;
   }
 }
