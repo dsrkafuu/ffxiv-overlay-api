84e0bdf6bdf94fbce8b17088379e344b02285f3a
dsrkafuu <dsrkafuu@outlook.com>
2021-11-21 17:04:51 +0800

    fix: update declaration file

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,112 +1,116 @@
-interface OverlayOptions {
-  extendData?: boolean;
-  silentMode?: boolean;
-  seperateLB?: boolean;
+declare namespace OverlayAPI {
+  export interface OverlayOptions {
+    extendData?: boolean;
+    silentMode?: boolean;
+    seperateLB?: boolean;
+  }
+
+  export interface EncounterData {
+    duration: string;
+    durationSeconds: number;
+    zoneName: string;
+    dps: number;
+    last10DPS: number;
+    last30DPS: number;
+    last60DPS: number;
+    hps: number;
+    damage: number;
+    healed: number;
+  }
+
+  export interface LimitBreakData {
+    name: 'Limit Break';
+    dps: number;
+    hps: number;
+    damage: number;
+    healed: number;
+    maxHit: string;
+    maxHeal: string;
+  }
+
+  export interface CombatantData {
+    name: string;
+    job: string;
+    jobType: 'dps' | 'heal' | 'tank' | 'others';
+    dps: number;
+    last10DPS: number;
+    last30DPS: number;
+    last60DPS: number;
+    hps: number;
+    swings: number;
+    hits: number;
+    deaths: number;
+    directHits: number;
+    directHitPct: string;
+    critHits: number;
+    critHitPct: string;
+    directCritHits: number;
+    directCritHitPct: string;
+    damage: number;
+    damageTaken: number;
+    damagePct: string;
+    healed: number;
+    healsTaken: number;
+    healsPct: string;
+    overHeal: number;
+    overHealPct: string;
+    maxHit: string;
+    maxHitDamage: number;
+    maxHeal: string;
+    maxHealDamage: number;
+  }
+
+  export interface ExtendData {
+    isActive: boolean;
+    encounter: EncounterData;
+    limitBreak?: LimitBreakData;
+    combatant: CombatantData[];
+  }
+
+  export type EventType =
+    | 'CombatData'
+    | 'LogLine'
+    | 'ImportedLogLines'
+    | 'ChangeZone'
+    | 'ChangePrimaryPlayer'
+    | 'OnlineStatusChanged'
+    | 'PartyChanged'
+    | 'BroadcastMessage';
+
+  export interface EventMessage {
+    type: EventType;
+    rseq?: number; // response promises for `callHandler`
+    [key: string]: any;
+  }
+
+  export type EventCallback = (msg: EventMessage, ...args: any[]) => any;
+
+  export type HandlerType =
+    | 'subscribe'
+    | 'getLanguage'
+    | 'getCombatants'
+    | 'saveData'
+    | 'loadData'
+    | 'say'
+    | 'broadcast';
+
+  export interface HandlerMessage {
+    call: HandlerType;
+    rseq?: number; // response promises for `callHandler`
+  }
 }
 
-interface EncounterData {
-  duration: string;
-  durationSeconds: number;
-  zoneName: string;
-  dps: number;
-  last10DPS: number;
-  last30DPS: number;
-  last60DPS: number;
-  hps: number;
-  damage: number;
-  healed: number;
-}
-
-interface LimitBreakData {
-  name: 'Limit Break';
-  dps: number;
-  hps: number;
-  damage: number;
-  healed: number;
-  maxHit: string;
-  maxHeal: string;
-}
-
-interface CombatantData {
-  name: string;
-  job: string;
-  jobType: 'dps' | 'heal' | 'tank' | 'others';
-  dps: number;
-  last10DPS: number;
-  last30DPS: number;
-  last60DPS: number;
-  hps: number;
-  swings: number;
-  hits: number;
-  deaths: number;
-  directHits: number;
-  directHitPct: string;
-  critHits: number;
-  critHitPct: string;
-  directCritHits: number;
-  directCritHitPct: string;
-  damage: number;
-  damageTaken: number;
-  damagePct: string;
-  healed: number;
-  healsTaken: number;
-  healsPct: string;
-  overHeal: number;
-  overHealPct: string;
-  maxHit: string;
-  maxHitDamage: number;
-  maxHeal: string;
-  maxHealDamage: number;
-}
-
-interface ExtendData {
-  isActive: boolean;
-  encounter: EncounterData;
-  limitBreak?: LimitBreakData;
-  combatant: CombatantData[];
-}
-
-type EventType =
-  | 'CombatData'
-  | 'LogLine'
-  | 'ImportedLogLines'
-  | 'ChangeZone'
-  | 'ChangePrimaryPlayer'
-  | 'OnlineStatusChanged'
-  | 'PartyChanged'
-  | 'BroadcastMessage';
-
-interface EventMessage {
-  type: EventType;
-  rseq?: number; // response promises for `callHandler`
-  [key: string]: any;
-}
-
-type EventCallback = (msg: EventMessage, ...args: any[]) => any;
-
-type HandlerType =
-  | 'subscribe'
-  | 'getLanguage'
-  | 'getCombatants'
-  | 'saveData'
-  | 'loadData'
-  | 'say'
-  | 'broadcast';
-
-interface HandlerMessage {
-  call: HandlerType;
-  rseq?: number; // response promises for `callHandler`
-}
-
-export default class OverlayAPI {
-  static mergeCombatant(...args: CombatantData[]): CombatantData;
-  constructor(options: OverlayOptions);
-  addListener(event: EventType, cb: EventCallback): void;
-  removeListener(event: EventType, cb: EventCallback): void;
-  removeAllListener(event: EventType): void;
-  getAllListener(event: EventType): EventCallback[];
+declare class OverlayAPI {
+  static mergeCombatant(...args: OverlayAPI.CombatantData[]): OverlayAPI.CombatantData;
+  constructor(options: OverlayAPI.OverlayOptions);
+  addListener(event: OverlayAPI.EventType, cb: OverlayAPI.EventCallback): void;
+  removeListener(event: OverlayAPI.EventType, cb: OverlayAPI.EventCallback): void;
+  removeAllListener(event: OverlayAPI.EventType): void;
+  getAllListener(event: OverlayAPI.EventType): OverlayAPI.EventCallback[];
   startEvent(): void;
   endEncounter(): Promise<void>;
-  callHandler(msg: HandlerMessage): Promise<EventMessage>;
-  simulateData(msg: EventMessage): void;
+  callHandler(msg: OverlayAPI.HandlerMessage): Promise<OverlayAPI.EventMessage>;
+  simulateData(msg: OverlayAPI.EventMessage): void;
 }
+
+export default OverlayAPI;
