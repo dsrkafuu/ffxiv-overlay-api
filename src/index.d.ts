e0d2e25e1f7de4a4679ac30b4e5f35bc83f656f2
dsrkafuu <dsrkafuu@outlook.com>
2021-11-25 20:25:15 +0800

    fix: ts declaration

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,106 @@
+/*! ffxiv-overlay-plugin TS declaration | DSRKafuU (https://dsrkafuu.net) | Copyright (c) MIT License */
+
+export interface OverlayOptions {
+  extendData?: boolean;
+  silentMode?: boolean;
+  seperateLB?: boolean;
+}
+
+export interface EncounterData {
+  duration: string;
+  durationSeconds: number;
+  zoneName: string;
+  dps: number;
+  last10DPS: number;
+  last30DPS: number;
+  last60DPS: number;
+  hps: number;
+  damage: number;
+  healed: number;
+}
+
+export interface LimitBreakData {
+  name: 'Limit Break';
+  dps: number;
+  hps: number;
+  damage: number;
+  healed: number;
+  maxHit: string;
+  maxHeal: string;
+}
+
+export interface CombatantData {
+  name: string;
+  job: string;
+  jobType: 'dps' | 'heal' | 'tank' | 'others';
+  dps: number;
+  last10DPS: number;
+  last30DPS: number;
+  last60DPS: number;
+  hps: number;
+  swings: number;
+  hits: number;
+  deaths: number;
+  directHits: number;
+  directHitPct: string;
+  critHits: number;
+  critHitPct: string;
+  directCritHits: number;
+  directCritHitPct: string;
+  damage: number;
+  damageTaken: number;
+  damagePct: string;
+  healed: number;
+  healsTaken: number;
+  healsPct: string;
+  overHeal: number;
+  overHealPct: string;
+  maxHit: string;
+  maxHitDamage: number;
+  maxHeal: string;
+  maxHealDamage: number;
+}
+
+export interface ExtendData {
+  isActive: boolean;
+  encounter: EncounterData;
+  limitBreak?: LimitBreakData;
+  combatant: CombatantData[];
+}
+
+export type EventType =
+  | 'CombatData'
+  | 'LogLine'
+  | 'ImportedLogLines'
+  | 'ChangeZone'
+  | 'ChangePrimaryPlayer'
+  | 'OnlineStatusChanged'
+  | 'PartyChanged'
+  | 'BroadcastMessage';
+
+export interface EventMessage {
+  type: EventType;
+  rseq?: number; // response promises for `callHandler`
+  [key: string]: any;
+}
+
+export type EventCallback = (msg: EventMessage, ...args: any[]) => any;
+
+export type HandlerType =
+  | 'subscribe'
+  | 'getLanguage'
+  | 'getCombatants'
+  | 'saveData'
+  | 'loadData'
+  | 'say'
+  | 'broadcast';
+
+export interface HandlerMessage {
+  call: HandlerType;
+  rseq?: number; // response promises for `callHandler`
+}
+
+// same as above within namespace
 declare namespace OverlayAPI {
   export interface OverlayOptions {
     extendData?: boolean;
@@ -100,6 +203,7 @@ declare namespace OverlayAPI {
   }
 }
 
+// root class
 declare class OverlayAPI {
   static mergeCombatant(...args: OverlayAPI.CombatantData[]): OverlayAPI.CombatantData;
   constructor(options: OverlayAPI.OverlayOptions);
@@ -113,4 +217,5 @@ declare class OverlayAPI {
   simulateData(msg: OverlayAPI.EventMessage): void;
 }
 
+// default export
 export default OverlayAPI;
